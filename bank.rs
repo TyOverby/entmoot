6a2e4cfef0f6923a1bf59eb57f92d2ebf9a6cae7
TyOverby <ty@pre-alpha.com>
2014-05-13 18:03:19 -0700

    implemented full storage and retrieval for entities in the world

--- bank.rs
+++ bank.rs
@@ -11,9 +11,10 @@ impl <C: Component> Bank<C> {
     pub fn new() -> Bank<C> {
         Bank { components: Vec::new(), holes: Vec::new() }
     }
+
     pub fn add(&mut self, c: C) -> uint {
         let mut pos:uint = 0;
-        if self.holes.is_empty() {
+        if !self.holes.is_empty() {
             pos = self.holes.pop().unwrap();
             let loc = self.components.get_mut(pos);
             *loc = c;
@@ -26,6 +27,10 @@ impl <C: Component> Bank<C> {
 
     pub fn del(&mut self, pos: uint) {
         self.holes.push(pos);
+    }
+
+    pub fn get<'a>(&'a mut self, pos: uint) -> &'a mut C {
+        self.components.get_mut(pos)
     }
 }
 
--- world.rs
+++ world.rs
@@ -5,6 +5,7 @@ extern crate core;
 
 use std::any::Any;
 use std::any::AnyMutRefExt;
+use std::any::AnyRefExt;
 
 use component::Component;
 use component::Sigl;
@@ -18,11 +19,17 @@ mod aspect;
 
 struct World {
     banks: Vec<Box<Any>>,
+    component_count: uint,
+    entity_id_counter: uint
 }
 
 impl World {
     pub fn new() -> World {
-        World { banks: Vec::new() }
+        World { banks: Vec::new(), component_count: 0, entity_id_counter: 0 }
+    }
+    pub fn spawn(&mut self) -> Entity {
+        self.entity_id_counter += 1;
+        Entity::new(self.entity_id_counter, self.component_count)
     }
     pub fn register_component<C: Component + 'static>(&mut self) {
         let id = Component::id(Sigl::<C>);
@@ -30,20 +37,22 @@ impl World {
         "Components must be registered in ascending order.");
         let any: Box<Any> = box Bank::<C>::new();
         self.banks.push(any);
+        self.component_count += 1;
     }
-    fn get_bank<'a, C: Component + 'static>(&'a mut self) -> (uint, Option<&'a mut Bank<C>>) {
+    fn get_bank<'a, C: Component + 'static>(&'a mut self) -> (uint, &'a mut Bank<C>) {
         let id = Component::id(Sigl::<C>);
         assert!(id < self.banks.len(), "Component has not been registered with World.");
         let bank_any: &'a mut Box<Any>  = self.banks.get_mut(id);
-        (id, bank_any.as_mut::<Bank<C>>())
+        let bank = match bank_any.as_mut::<Bank<C>>() {
+            Some(e) => e,
+            None => fail!("Internal Error: Found bank of wrong type.")
+        };
+        (id, bank)
     }
 
     pub fn map_component<C: Component + 'static>(&mut self, component: C, entity: &mut Entity) {
-        let (component_id, bank_opt) = self.get_bank::<C>();
-        let bank = match bank_opt {
-            Some(b) => b,
-            None => fail!("Internal Error: Found bank of wrong type.")
-        };
+        let (component_id, bank) = self.get_bank::<C>();
+        // TODO: check to see if the entity already has this component
         let pos = bank.add(component);
         {
             let offsets = entity.offsets();
@@ -59,12 +68,19 @@ impl World {
         }
     }
 
+    pub fn get_component<'a, C: Component + 'static>(&'a mut self, entity: &mut Entity) -> Option<&'a mut C>{
+        let (component_id, bank) = self.get_bank::<C>();
+        let offset = entity.offsets().get(component_id);
+        if *offset == std::uint::MAX {
+            return None;
+        } else {
+            return Some(bank.get(*offset));
+        }
+
+    }
+
     pub fn unmap_component<C: Component + 'static>(&mut self, entity: &mut Entity) {
-        let (id, bank_opt) = self.get_bank::<C>();
-        let bank = match bank_opt {
-            Some(b) => b,
-            None => fail!("Internal Error: Found bank of wrong type.")
-        };
+        let (id, bank) = self.get_bank::<C>();
         bank.del(entity.id());
         entity.aspect().del_key(id);
     }
@@ -74,9 +90,26 @@ impl World {
 mod world_tests {
     use super::World;
     use entity::Entity;
+    use component::Component;
+    use component::Sigl;
 
     #[test]
     fn test_component_map() {
+        struct Pos {
+            x: uint, y: uint
+        }
+        impl Component for Pos {
+            fn id(_: Sigl<Pos>) -> uint {0}
+        }
         let mut world = World::new();
+        world.register_component::<Pos>();
+
+        let mut e1 = world.spawn();
+        world.map_component(Pos {x: 0, y: 5}, &mut e1);
+        let c1 = world.get_component::<Pos>(&mut e1);
+        assert!(c1.is_some());
+        let c1 = c1.unwrap();
+        assert!(c1.x == 0);
+        assert!(c1.y == 5);
     }
 }
